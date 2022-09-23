9facb996ac620f86db708876afa955700199ae6f
LBONTEN <lennyb.0908@gmail.com>
2022-09-23 16:49:01 +0200

    first steps

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,9 @@
 FROM semtech/mu-python-template:2.0.0-beta.1
+LABEL maintainer="lennyb.0908@gmail.com"
+
+
+ENV FILE_RESOURCE_BASE=http://mu.semte.ch/services/file-service/files/
+ENV MU_APPLICATION_FILE_STORAGE_PATH=files/toExtract/
+ENV MU_SPARQL_ENDPOINT=http://database:8890/sparql
+ENV MU_SPARQL_TIMEOUT=60
+ENV LOG_LEVEL=info
--- web.py
+++ web.py
@@ -1,3 +1,62 @@
+""" Service to extract text from a file using Tika
+`	Author: Bontenakel Lenny - lennyb.0908@gmail.com
+	Created: 23 Septembre 2022
+"""
+
+from urllib import request
+from string import Template
+from helpers import query
+from escape_helpers import sparql_escape_uri
+
+import tika # https://github.com/chrismattmann/tika-python
+tika.initVM()
+from tika import parser
+
+
+# TODO delete route this later
 @app.route("/hello")
 def hello():
     return "Hello from the mu-python-template!"
+
+""" receive a logical file and attach the extracted text from it
+	Files are uploaded to this endpoint using the mu-semtech/file-service (https://github.com/mu-semtech/file-service)
+	@file: the file from which to extract text
+"""
+@app.route("/extract", methods=['POST'])
+def extractText():
+	 
+	file = request.args.get("file")
+	
+	extractedText = parser.from_file(filepath)
+	return extractedText
+	
+	
+""" save text from a file into a triple store
+ 
+"""
+def saveExtractedText(fileId, extractedText):
+	query_template = Template("""
+	PREFIX files: <http://lynx.lblod.info/files/>
+	PREFIX ext: <http://mu.semte.ch/vocabularies/ext/> 
+
+	INSERT DATA { GRAPH {
+		files:$fileId ext:extractedText $extractedText .froggy 
+		} 
+	}
+	""")
+	query_string = query_template.substitute(
+		fileId=sparql_escape_uri(fileId),
+		extractedText=sparql_escape_uri(extractedText)
+	)
+	
+	query_result = query(query_string)
+	return query_result
+	
+	
+""" receive a triple and search the database what to extract
+	
+"""
+@app.route("/delta", methods=['POST'])
+def receiveTriple():
+	return "where is the triple?"
+	
